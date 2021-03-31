8543ff511da7fa6c1aab619d9dde44bd976fb61c
Fredrik bjornland <fredrik.bjornland@vikenfiber.no>
2021-03-31 12:44:18 +0200

    add transform

--- pca.py
+++ pca.py
@@ -1,5 +1,19 @@
+import csv
+import numpy as np
 
 class PCA:
-
     def __init__(self):
-        pass
+        self.swiss_array = self.csv_reader("swiss_data.csv")
+
+    def transform(self, x):
+        x = x - np.mean(self.swiss_array, 0)
+        return np.matmul(np.transpose(F), x)
+
+    def csv_reader(self, file):
+        with open(file) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
+        return csv_reader
+
+if __name__ == '__main__':
+    pca = PCA()
+    pca.csv_reader("swiss_data.csv")
