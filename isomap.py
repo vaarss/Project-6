5810643fc3d21de110ea8385ba7cb8db6397b4a7
Fredrik bjornland <fredrik.bjornland@vikenfiber.no>
2021-04-06 18:49:34 +0200

    ferdig

--- isomap.py
+++ isomap.py
@@ -1,23 +1,77 @@
-import numpy as np
-class Isomap:
+""" isomap file """
 
-    def __init__(self, data_points, k): 
+import numpy as np
+from sklearn.utils.graph import graph_shortest_path
+import scipy.sparse.linalg
+import matplotlib.pyplot as plt
+
+class Isomap:
+    """ Isomap constructor """
+    def __init__(self, data_points, k):
+        """ constructor """
         self.data_points = data_points
         self.k = k
-        self.euclidian_distances()
         
-    def euclidian_distances(self):
-        N, D = self.data_points.shape
-        euclidian_matrix = np.zeros((N, N))
-        for i in self.data_points:
-            pass
+    def euclidian_distances(self, data_points):
+        """Create euclidian distances matrix """
+        x = np.square(data_points)
+        v = np.sum(x, axis=1, keepdims=True)
+        W = -2 * data_points @ data_points.transpose()
+        d_2 = v.transpose() + v + W
+        d = np.sqrt(np.abs(d_2))
+        return d
 
-    def euclidian_distance(self, point_i, point_j, d):
-        summer = 0
-        for i in range(d):
-            summer += (point_i[i]-point_j[i])**2
+    def keep_k_neareset(self, distance_matrix, k):
+        """ Only keep the k nearest values """
+        holder = distance_matrix
+        sorted_indexes = holder.argsort(axis=1)
+        sorted_indexes = np.delete(sorted_indexes, slice(k + 1), 1)
+        y_dim = distance_matrix.shape[1]
+        if k < y_dim:
+            for i in range(y_dim):
+                distance_matrix[i][sorted_indexes[i]] = 0
+        return distance_matrix
 
+    def mds(self, geodesic):
+        """ Multidimensional scaling from the taskpaper"""
+        d = np.square(geodesic)
+        n = d.shape[0]
+        identity_matrix = np.identity(n)
+        column_ones = np.ones((n, 1))
+        # j er centering matrix, denne er riktig
+        j = identity_matrix - ((1/n)*(column_ones @ column_ones.T))
+        
+        b = (-0.5) *j @ d @ j
+        # Eigenvalues are sorted
+        eigenvalues, eigenvectors = np.linalg.eig(b)  # FØLER eigh er feil, skrev eig istden
+
+        sorted_indexes = eigenvalues.argsort()[::-1]
+        eigenvalues = np.real(eigenvalues[sorted_indexes][:2])
+        eigenvectors = (eigenvectors.transpose()[sorted_indexes]).transpose()
+        eigenvectors = np.real(eigenvectors[:, :2])
+        a = np.zeros((2, 2))
+        np.fill_diagonal(a, eigenvalues)
+        a_power_of_0_point_5 = scipy.linalg.fractional_matrix_power(a, 0.5)
+        Y = eigenvectors @ a_power_of_0_point_5
+        return Y
+
+    def iso(self):
+        distance_matrix = self.euclidian_distances(self.data_points)
+        distance_matrix = self.keep_k_neareset(distance_matrix, self.k)
+        shortest_path = graph_shortest_path(distance_matrix)
+        Y = self.mds(shortest_path)
+        return Y
 
 if __name__ == '__main__':
-    datapoints = np.genfromtxt(("swiss_data.csv"), delimiter = ',')
-    iso = Isomap(datapoints, 10)
+    inputFiles = ["swiss_data.csv", "digits.csv"]
+    inputIndex = 0
+
+    datapoints = np.genfromtxt((inputFiles[inputIndex]), delimiter = ',')
+    iso = Isomap(datapoints, 30)
+    Y = iso.iso()
+    if inputIndex == 0:
+         C = np.arange(Y.shape[0])
+    else:
+        C=np.genfromtxt('digits_label.csv')
+    plt.scatter(Y[:,0], Y[:,1], s=10, c=C, cmap="jet")
+    plt.show()
--- pca.py
+++ pca.py
@@ -1,50 +1,53 @@
+""" pca file """
 import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import scipy.sparse.linalg
 
 class PCA:
-    def __init__(self):
-        pass
-
-
+    """ PCA class """
     def fit(self, datapoints, dimension_in, dimension_out):
-        datapoints = datapoints -  datapoints.mean(axis=0)
+        """ fit method """
+        #Center datapoints
+        datapoints = datapoints -  datapoints.mean(axis=0, keepdims=True)
         matrix = np.cov(np.transpose(datapoints))
         if dimension_in - 1 > dimension_out:
             [eigenvalues, eigenvectors] = scipy.sparse.linalg.eigs(matrix)
         elif dimension_in -1 == dimension_out: 
             eigenvalues, eigenvectors = np.linalg.eigh(matrix)
-        idx = eigenvalues.argsort()[::-1]
-        eigenvectors=eigenvectors[:, idx[-dimension_out:]]
-        eigenvectors = np.array(eigenvectors)
+        sorted_indexes = eigenvalues.argsort()
+        eigenvectors=eigenvectors[:, sorted_indexes[-dimension_out:]]
         return eigenvectors
 
 
-    def transform(self, x, datapoints):
+    def transform(self, x, datapoints, dimension_in, dimension_out):
+        """ Transform from tasksheet"""
         x = x - np.mean(datapoints, axis=0)
-        dimension_in = len(x)
-        transformation_matrix = self.fit(datapoints, dimension_in, 2)
-        return_value = np.matmul(np.transpose(transformation_matrix), x)
+        transformation_matrix = self.fit(datapoints, dimension_in, dimension_out)
+        return_value = np.transpose(transformation_matrix) @ x
         return return_value[np.newaxis]
 
 
     def transform_matrix(self, datapoints):
+        """ Transform every datapoint """
         arr = np.zeros((0,2))
+        dimension_in = datapoints.shape[1]
         for i in datapoints:
-            arr = np.append(arr, self.transform(i, datapoints), axis=0)
+            arr = np.append(arr, self.transform(i, datapoints,dimension_in, 2), axis=0)
         return arr
 
 if __name__ == '__main__':
+    """ main """
     pca = PCA()
-    datapoints = np.genfromtxt(("swiss_data.csv"), delimiter = ',')
-    transformed = pca.transform_matrix(datapoints)
-    x = []
-    y = []
-    for data_point in transformed:
-        x.append(data_point[0])
-        y.append(data_point[1])
-    N = datapoints.shape[0]
-    plt.scatter(x,y, c=np.arange(N))
+    inputFiles = ["swiss_data.csv", "digits.csv"]
+    inputIndex = 0
+    
+    datapoints = np.genfromtxt((inputFiles[inputIndex]), delimiter = ',')
+    Y = pca.transform_matrix(datapoints)
+    if inputIndex == 0:
+         C = np.arange(Y.shape[0])
+    else:
+        C=np.genfromtxt('digits_label.csv')
+    plt.scatter(Y[:,0], Y[:,1], s=10, c=C, cmap="jet")
     plt.show()
 
